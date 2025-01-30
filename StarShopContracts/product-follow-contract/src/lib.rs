36b5f633a0102d6cac52aeee3b8027837f435816
frankie-powers <franklin.power21@gmail.com>
2025-01-30 05:23:14 +0100

    feat: added  lib.rs

--- StarShopContracts/product-follow-contract/src/lib.rs
+++ StarShopContracts/product-follow-contract/src/lib.rs
@@ -1,1 +1,14 @@
+#![no_std]
+use soroban_sdk::contract;
 
+mod alerts;
+mod datatype;
+mod follow;
+mod interface;
+mod notification;
+
+#[cfg(test)]
+mod test;
+
+#[contract]
+pub struct ProductFollowContract;
